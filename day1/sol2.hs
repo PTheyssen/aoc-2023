18729449991ea89366a0e7b34bca66ae2904fd0b
Philipp Theyssen <p.theyssen@gmail.com>
2023-12-03 16:55:50 +0100

    ouch Text ouch String

--- day1/sol2.hs
+++ day1/sol2.hs
@@ -1,37 +1,36 @@
-
+{-# LANGUAGE OverloadedStrings #-}
 import Data.List
 import Data.List.Split
 import Data.Maybe
 import Text.Read -- readMaybe
+import qualified Data.Text as T
+import Data.Text.Internal.Search
 
 main = do
   contents <- readFile "input.txt"
-  print $ sum $ map getCalibrationValue' $ (lines (contents))
+  -- print $ map getCalibrationValue' $ (lines (contents))
+  print $ sum $ map (getCalibrationValue'. T.pack) $ (lines (contents))
 
-getCalibrationValue' :: String -> Int
+getCalibrationValue' :: T.Text -> Int
 getCalibrationValue' s = read d :: Int
   where
     d = concat $ map show $ getCalibrationValue s
 
-a = getCalibrationValue' "two1nine"
-b = getCalibrationValue' "eightwothree"
-c = getCalibrationValue' "abcone2threexyz"
-d = getCalibrationValue' "xtwone3four"
-f = getCalibrationValue' "4nineeightseven2"
-e = getCalibrationValue' "zoneight234"
-g = getCalibrationValue' "7pqrstsixteen"
+x1 = getCalibrationValue' "fourthreetpmqqtzgtwofour"
+x2 = getCalibrationValue' "1fourthreetpmqqtzgtwofour"
+s1 = findSpelledOutDigits  "1fourthreetpmqqtzgtwofour"
 
-getCalibrationValue :: String -> [Int]
+getCalibrationValue :: T.Text -> [Int]
 getCalibrationValue s =
   case length listOfDigits of
     1 -> listOfDigits ++ listOfDigits
     2 -> listOfDigits
     _ -> [(head listOfDigits)] ++ [last listOfDigits]
   where
-    listOfDigits = concat $ map t $ split (oneOf ['1'..'9']) s -- list of
+    listOfDigits = concat $ map t $ split (oneOf ['1'..'9']) (T.unpack s) -- list of
     t = \x -> case readMaybe x :: Maybe Int of
                 Just d -> [d]
-                Nothing -> findSpelledOutDigits x
+                Nothing -> findSpelledOutDigits (T.pack x)
            
 
 -- part 2 digits spelled out in letters
@@ -43,16 +42,17 @@ findString search str = findIndex (isPrefixOf search) (tails str)
 compFirstElem :: (Int, Int) -> (Int, Int) -> Ordering
 compFirstElem (x1, _) (x2, _) = compare x1 x2
 
-findSpelledOutDigits :: String -> [Int]
+findSpelledOutDigits :: T.Text -> [Int]
 findSpelledOutDigits s =
-  map snd $ sortBy compFirstElem $ mapMaybe (\x -> tmp s x) spelledOut
+  map snd $ sortBy compFirstElem $ concat $ map (\x -> tmp s x) spelledOut
   -- combine and order
   where
     spelledOut = [("one", 1), ("two", 2), ("three", 3),
                   ("four", 4), ("five", 5), ("six", 6),
                   ("seven", 7), ("eight", 8), ("nine", 9)]
 
-tmp :: String -> (String, Int) -> Maybe (Int, Int)
-tmp s tup = case findString (fst tup) s of
-              Nothing -> Nothing
-              Just pos -> Just (pos, snd tup)
+-- findString needs all occurences!
+tmp :: T.Text -> (T.Text, Int) -> [(Int, Int)]
+tmp s tup = case indices (fst tup) s of
+              x -> map (\y -> (y, snd tup)) x
+
